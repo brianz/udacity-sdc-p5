9f9c032a4ec44a827e0d5e3827690f5f06c79440
Brian Zambrano <brianz@gmail.com>
2017-03-24 21:53:30 -0600

    cleanup

--- rects.py
+++ rects.py
@@ -30,31 +30,23 @@ def draw_boxes(img, bboxes, color=(0, 0, 255), thick=6):
     return imcopy
 
 
-# Define a function that takes an image,
-# start and stop positions in both x and y,
-# window size (x and y dimensions),
-# and overlap fraction (for both x and y)
 def slide_window(img,
                  x_start_stop=[None, None],
                  y_start_stop=[None, None],
                  xy_window=(64, 64),
                  xy_overlap=(0.5, 0.5)
                 ):
-    # If x and/or y start/stop positions not defined, set to image size
+    x_window_size, y_window_size = xy_window
+
+    x_step, y_step = map(int, np.array(xy_window) * np.array(xy_overlap))
+
     if not all(x_start_stop):
-        x_start_stop = [0, img.shape[1]]
+        x_start_stop = [0, img.shape[1] - x_window_size + 1]
     if not all(y_start_stop):
-        y_start_stop = [0, img.shape[0]]
+        y_start_stop = [0, img.shape[0] - y_window_size + 1]
 
-    x_boundary = x_start_stop[1]
-    y_boundary = y_start_stop[1]
-
-    step_sizes = np.array(xy_window) * np.array(xy_overlap)
-    x_step = int(step_sizes[0])
-    y_step = int(step_sizes[1])
-
-    x_ranges = map(int, x_start_stop + [step_sizes[0]])
-    y_ranges = map(int, y_start_stop + [step_sizes[1]])
+    x_ranges = map(int, x_start_stop + [x_step])
+    y_ranges = map(int, y_start_stop + [y_step])
 
     x_steps = range(*x_ranges)
     y_steps = range(*y_ranges)
@@ -62,14 +54,10 @@ def slide_window(img,
     window_list = []
     for y in y_steps:
         for x in x_steps:
-            if x + xy_window[0] > x_boundary or \
-                    y + xy_window[1] > y_boundary:
-                continue
             window_list.append((
                 (x, y),
-                (x + xy_window[0], y + xy_window[1])
+                (x + x_window_size, y + y_window_size)
             ))
-        break
 
     return window_list
 
